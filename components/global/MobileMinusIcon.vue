f552f48306093f44c762fadb3ad23c83de867d20
ProSky <131782642+LorenzoProSky@users.noreply.github.com>
2024-06-10 22:24:07 +0200

    Updated mobile icons

---
+++ components/global/MobileMinusIcon.vue
@@ -0,0 +1,13 @@
+<!-- Mobile Minus Icon - Vue component for module 'nuxt-icon' needs to be inside components/global/ folder -->
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'MobileMinusIcon',
+});
+</script>
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256">
+    <path fill="currentColor" d="M228 128a12 12 0 0 1-12 12H40a12 12 0 0 1 0-24h176a12 12 0 0 1 12 12" />
+  </svg>
+</template>
---
+++ components/global/MobilePlusIcon.vue
@@ -0,0 +1,14 @@
+<!-- Mobile Plus Icon - Vue component for module 'nuxt-icon' needs to be inside components/global/ folder -->
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'MobilePlusIcon',
+});
+</script>
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256">
+    <path fill="currentColor"
+      d="M228 128a12 12 0 0 1-12 12h-76v76a12 12 0 0 1-24 0v-76H40a12 12 0 0 1 0-24h76V40a12 12 0 0 1 24 0v76h76a12 12 0 0 1 12 12" />
+  </svg>
+</template>
