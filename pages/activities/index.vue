ad6118fafcf2970b2921a82436e992f37de01cbe
Ivan Della Vecchia <dellavecchiaivan@gmail.com>
2024-06-14 12:02:24 +0200

    Pull of the activity page

--- pages/activities/index.vue
+++ pages/activities/index.vue
@@ -1,7 +1,5 @@
 <script lang="ts" setup>
-// Import necessary assets and components
-import activitiesCoverImage from '~/assets/images/our-activities-index-cover.png';
-import activitiesQuoteImage from '~/assets/images/our-activities-index-quote.png';
+// Import necessary components
 import DiscoverCard from '~/components/cards/DiscoverCard.vue';
 </script>
 
@@ -9,24 +7,23 @@ import DiscoverCard from '~/components/cards/DiscoverCard.vue';
 <template>
   <div class="activities-page">
     <!-- Cover section with image and title -->
-    <div class="cover-section">
-      <img loading="lazy" :src="activitiesCoverImage" class="cover-image" />
-      <h1 class="cover-title">Explore<br />Our Activities</h1>
+    <div class="page-title">
+      <h1 class="title-text">Explore<br />Our Activities</h1>
     </div>
 
-    <!-- Section title -->
-    <h3 class="section-title">MiLA Activities</h3>
-
-    <!-- Description of activities -->
-    <div class="section-description">
-      At MiLA, we are dedicated to helping women and children affected by
-      domestic violence through a variety of impactful services and projects.<br />Explore
-      our activities to see how we can support your journey to a safer life.
+    <!-- Section content -->
+    <div class="page-section">
+      <h3 class="section-title">MiLA Activities</h3>
+      <div class="section-description">
+        At MiLA, we are dedicated to helping women and children affected by
+        domestic violence through a variety of impactful services and projects.<br />Explore
+        our activities to see how we can support your journey to a safer life.
+      </div>
     </div>
 
-    <!-- Cards displaying services and projects -->
-    <div class="activities-cards">
-      <div class="card-container">
+    <!-- Cards for discovering services and projects -->
+    <div class="cards-container">
+      <div class="page-cards">
         <DiscoverCard title="Discover all the Services"
           text="Discover our wide array of supportive services, including counseling, legal assistance, and safe housing, all aimed at empowering women to build safe, empowered and independent lives."
           to="/activities/services/index" />
@@ -39,18 +36,15 @@ import DiscoverCard from '~/components/cards/DiscoverCard.vue';
     <!-- Quote section -->
     <div class="quote-section">
       <div class="quote-container">
-        <img loading="lazy" :srcSet="activitiesQuoteImage" class="quote-image" />
+        <Icon class="quote-icon" name="QuoteIcon" size="85" />
         <div class="quote-content">
-          <div class="quote-icon-wrapper">
-            <Icon name="QuoteIcon" size="85" class="icon-quote" />
+          <div class="quote-text">
+            At MiLA, we understand the immense courage it takes to seek help. We
+            are here to listen, support, and empower you every step of the way.
+            Together, we can build a brighter, safer future.
           </div>
-          <div class="quote-text-wrapper">
-            <div class="quote-text">
-              At MiLA, we understand the immense courage it takes to seek help. We
-              are here to listen, support, and empower you every step of the way.
-              Together, we can build a brighter, safer future.
-            </div>
-            <div class="quote-author">Sofia Conti, Centre Coordinator</div>
+          <div class="quote-author">
+            Sofia Conti, Centre Coordinator
           </div>
         </div>
       </div>
@@ -60,253 +54,160 @@ import DiscoverCard from '~/components/cards/DiscoverCard.vue';
 
 <!-- Scoped styles for the activities page -->
 <style scoped>
-/* General page styling */
 .activities-page {
+  width: 100%;
   display: flex;
   flex-direction: column;
-  align-items: center;
 }
 
-@media (max-width: 997px) {
-  .activities-page {
-    max-width: 100%;
-  }
-}
-
-/* Cover section styling */
-.cover-section {
-  flex-direction: column;
-  overflow: hidden;
-  align-self: stretch;
-  position: relative;
-  display: flex;
-  min-height: 960px;
+.page-title {
+  background-image: url('/assets/images/our-activities-index-cover.png');
+  background-size: cover;
   width: 100%;
-  justify-content: center;
+  height: calc(100vw / 2);
 }
 
-@media (max-width: 997px) {
-  .cover-section {
-    max-width: 100%;
-  }
-}
-
-/* Cover image styling */
-.cover-image {
-  position: absolute;
-  inset: 0;
-  height: 100%;
-  width: 100%;
-  object-fit: cover;
-  object-position: center;
-}
-
-/* Cover title styling */
-.cover-title {
-  position: relative;
+.title-text {
   font-family: var(--font-playfair);
   font-weight: var(--extrabold);
+  font-size: var(--h1);
   color: var(--white);
-  width: 100%;
-  align-items: start;
-  justify-content: center;
-  padding: 475px 60px 197px 189px;
 }
 
-@media (max-width: 997px) {
-  .cover-title {
-    max-width: 100%;
-    font-size: var(--h3);
-    line-height: 44px;
+h1{
+  position: relative;
+  bottom: -50%;
+  left: 10%;
+  z-index: 1;
+  width:fit-content;
+}
+
+@media (max-width: 1400px) {
+  .title-text {
+    margin-top: -5vw;
   }
 }
 
-/* Section title styling */
-.section-title {
+.page-section {
+  margin: 160px auto;
+  margin-bottom: 0px;
+  width: 50vw;
   color: var(--black);
+}
+
+@media (max-width: 1500px) {
+  .page-section {
+    margin-top: 80px;
+  }
+}
+
+.section-title {
   text-align: center;
-  margin-top: 172px;
+  margin-top: 0px;
   font-family: var(--font-playfair);
   font-weight: var(--bold);
   font-size: 42px;
 }
 
-@media (max-width: 997px) {
-  .section-title {
-    margin-top: 40px;
-  }
-}
-
-/* Description of activities styling */
 .section-description {
-  color: var(--black);
   text-align: center;
   margin-top: 32px;
-  width: 922px;
   font-family: var(--font-montserrat);
   font-weight: var(--regular);
   font-size: var(--body4);
 }
 
-@media (max-width: 997px) {
-  .section-description {
-    max-width: 100%;
-    margin-top: 40px;
-  }
-}
-
-/* Cards displaying services and projects styling */
-.activities-cards {
-  margin-top: 163px;
-  width: 100%;
-  max-width: 1360px;
-}
-
-@media (max-width: 997px) {
-  .activities-cards {
-    max-width: 100%;
-    margin-top: 40px;
-  }
-}
-
-/* Container for cards styling */
-.card-container {
-  gap: 160px;
+.cards-container {
+  margin-top: 160px;
+  margin-bottom: 0px;
   display: flex;
-  justify-content: center;
-}
-
-@media (max-width: 997px) {
-  .card-container {
-    flex-direction: column;
-    align-items: stretch;
-    gap: 10px;
-  }
-}
-
-/* Column layout adjustment for smaller screens */
-.column {
-  display: flex;
-  flex-direction: column;
-  line-height: normal;
-  width: 50%;
-  margin-left: 0px;
-}
-
-@media (max-width: 997px) {
-  .column {
-    width: 100%;
-  }
-}
-
-/* Quote section styling */
-.quote-section {
-  align-self: stretch;
-  display: flex;
-  overflow: hidden;
-  margin-top: 330px;
-  margin-bottom: 345px;
-  width: 100%;
   flex-direction: column;
   align-items: center;
+}
+
+@media (max-width: 1500px) {
+  .cards-container {
+    margin-top: 80px;
+  }
+}
+
+.page-cards {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 8vw;
+}
+
+@media (max-width: 1500px) {
+  .page-cards {
+    flex-direction: column;
+    gap: 80px;
+  }
+}
+
+.quote-section {
+  margin-top: 330px;
+  margin-bottom: 345px;
+  background-image: url('/assets/images/our-activities-index-quote.png');
+  background-size: cover;
+  width: 100%;
+  height: calc(69.5vw / 2);
   color: var(--black);
 }
 
-@media (max-width: 997px) {
+@media (max-width: 1500px) {
   .quote-section {
-    max-width: 100%;
-    margin-top: 40px;
+    margin-top: 165px;
+    margin-bottom: 172.5px;
   }
 }
 
-/* Quote container styling */
 .quote-container {
-  flex-direction: column;
-  overflow: hidden;
-  position: relative;
   display: flex;
-  min-height: 678px;
+  flex-direction: row;
+  align-items: start;
+  margin: 7.2vw 10.42vw 7.34vw 59.7vw;
+  width: 30vw;
+}
+
+.quote-icon {
   width: 100%;
-  max-width: 1920px;
-  align-items: end;
+  color: var(--purple);
 }
 
-@media (max-width: 997px) {
-  .quote-container {
-    max-width: 100%;
-    padding-left: 20px;
-  }
-}
-
-/* Quote image styling */
-.quote-image {
-  position: absolute;
-  inset: 0;
-  height: 100%;
-  width: 100%;
-  object-fit: cover;
-  object-position: center;
-  display: flex;
-}
-
-/* Quote content styling */
 .quote-content {
-  position: relative;
+  margin: 1.563vw 0 0 0;
   display: flex;
-  align-items: flex-start;
-  gap: 0px;
-  padding: 141px 194px 141px 1167px;
-  margin-top: 35px;
+  flex-direction: column;
 }
 
-/* Quote icon wrapper styling */
-.quote-icon-wrapper {
-  position: relative;
-  margin-top: -35px;
-}
-
-@media (max-width: 998px) {
-  .quote-content {
-    flex-wrap: wrap;
-    padding: 0px;
-  }
-}
-
-/* Quote text wrapper styling */
-.quote-text-wrapper {
-  flex-grow: 1;
-  flex-basis: auto;
-}
-
-/* Quote text styling */
 .quote-text {
   font-family: var(--font-montserrat);
-  font-size: var(--h4);
   font-weight: var(--regular);
-  align-self: end;
-  flex-grow: 1;
-  flex-basis: auto;
+  font-size: var(--h4);
 }
 
-@media (max-width: 997px) {
-  .quote-text {
-    max-width: 100%;
-  }
-}
-
-/* Quote author styling */
 .quote-author {
+  margin-top: 2.1vw;
   font-family: var(--font-montserrat);
-  font-size: var(--body3);
   font-weight: var(--medium);
+  font-size: var(--body3);
   font-style: italic;
-  position: relative;
-  margin-top: 42px;
 }
 
-@media (max-width: 997px) {
-  .quote-author {
-    margin: 40px 0;
+@media (max-width: 1700px) {
+  .quote-container {
+    margin: 5vw 0 10vw -10vw;
+    width: 100%;
+  }
+
+  .quote-icon {
+    min-width: 100%;
+    margin: -3% -35% 0 -25% ;
+  }
+
+  .quote-content {
+    margin: 0 0 0 -10vw;
   }
 }
 </style>
